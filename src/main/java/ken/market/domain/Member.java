a72e9fec22656f0ca6742db38e9264ff2c9ae267
kenryu <jryu1521@gmail.com>
2024-03-12 17:21:06 +0900

    database

--- src/main/java/ken/market/domain/Member.java
+++ src/main/java/ken/market/domain/Member.java
@@ -1,22 +1,22 @@
 package ken.market.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class Member {
-    private Long id;
-    private String name;
+    private int memberCode;
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String password;
+    private List<Post> posts;
+    private double rate;
+    private List<Review> reviews;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
+
